02217967fd5273fc52d5fd8916d313cd91b51ab4
brunjlar <brunjlar@gmail.com>
2016-09-24 00:36:43 +0200

    hello world

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,60 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
+
+import Control.Distributed.Process
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node                   (initRemoteTable)
+import Control.Monad
+import Data.Binary                                        (Binary)
+import Data.Typeable                                      (Typeable)
+import GHC.Generics                                       (Generic)
+import System.Environment
+
+data GalaxyMessage = LookForGalaxy ProcessId | GalaxyFound String
+    deriving (Show, Typeable, Generic)
+
+instance Binary GalaxyMessage
+
+traveller :: Process ()
+traveller = do
+    say "here's a traveller!"
+    LookForGalaxy pid <- expect
+    say $ "received message from " ++ show pid
+    send pid $ GalaxyFound "Andromeda"
+    say "traveller sent message"
+
+remotable ['traveller]
+
+master :: Backend -> [NodeId] -> Process ()
+master backend nodes = do
+    say $ "here's the master!"
+    myPid <- getSelfPid
+    forM_ nodes $ \node -> do
+        nodePid <- spawn node $(mkStaticClosure 'traveller)
+        say $ "slave " ++ show node ++ " spawned"
+        send nodePid $ LookForGalaxy myPid
+        say $ "master sent message to " ++ show nodePid
+        GalaxyFound n <- expect
+        say $ "found galaxy '" ++ n ++ "'"
+        terminateAllSlaves backend
+        say $ "slaves terminated"
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    args <- getArgs
+    case args of
+      ["master", host, port] -> do
+          backend <- initializeBackend host port initRemoteTable
+          putStrLn "backend initialized"
+          startMaster backend (master backend)
+          putStrLn "master started"
+      ["slave", host, port]  -> do
+          backend <- initializeBackend host port initRemoteTable
+          putStrLn "backend initialized"
+          startSlave backend
+          putStrLn "slave started"
+      _                      -> putStrLn "unknown parameters"
