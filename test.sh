4de4199ad601bb400b495e254b752b2e258e7ae6
brunjlar <brunjlar@gmail.com>
2016-09-26 01:35:58 +0200

    first try

--- test.sh
+++ test.sh
@@ -5,17 +5,17 @@ stack install
 
 distributed slave localhost 8081 &
 distributed slave localhost 8082 &
-distributed slave localhost 8083 &
-distributed slave localhost 8084 &
-distributed slave localhost 8085 &
-distributed slave localhost 8086 &
-distributed slave localhost 8087 &
-distributed slave localhost 8088 &
-distributed slave localhost 8089 &
-distributed slave localhost 8090 &
+#distributed slave localhost 8083 &
+#distributed slave localhost 8084 &
+#distributed slave localhost 8085 &
+#distributed slave localhost 8086 &
+#distributed slave localhost 8087 &
+#distributed slave localhost 8088 &
+#distributed slave localhost 8089 &
+#distributed slave localhost 8090 &
 
 echo slaves started
 echo
 sleep 1
 
-distributed master localhost 8091
+distributed master localhost 8091 123456 1 20
